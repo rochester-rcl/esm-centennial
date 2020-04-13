6099e5be9f021c748096c466c6fad30806c4e403
jjromphf <jromphf@library.rochester.edu>
2020-04-13 16:57:32 -0400

    initial commit of audio work

---
+++ Assets/Scripts/AudioTools/MultiTrackPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace AudioTools
+{
+    public class MultiTrackPlayer : MonoBehaviour
+    {
+        public AudioClip[] tracks;
+        public bool loop;
+        public bool playOnAwake;
+        public AudioMixerGroup output;
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        void Awake()
+        {
+            AttachAudioSources();
+        }
+
+        private void AttachAudioSources()
+        {
+            foreach (AudioClip track in tracks)
+            {
+                AudioSource source = gameObject.AddComponent<AudioSource>();
+                source.clip = track;
+                source.loop = loop;
+                source.outputAudioMixerGroup = output;
+                if (playOnAwake) source.Play();
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
---
+++ Assets/Scripts/AudioTools/MultiTrackPlayerEditorLayout.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+namespace AudioTools
+{
+#if UNITY_EDITOR
+    [CustomEditor(typeof(MultiTrackPlayer))]
+    public class MultiTrackPlayerEditorLayout : Editor
+    {
+        public SerializedProperty tracks;
+        public SerializedProperty trackArraySize;
+        public SerializedProperty loop;
+        public SerializedProperty playOnAwake;
+        public SerializedProperty output;
+
+        private int buttonWidth = 100;
+        private Rect lastRect;
+
+        public void OnEnable()
+        {
+            tracks = serializedObject.FindProperty("tracks");
+            trackArraySize = tracks.FindPropertyRelative("Array.size");
+            loop = serializedObject.FindProperty("loop");
+            playOnAwake = serializedObject.FindProperty("playOnAwake");
+            output = serializedObject.FindProperty("output");
+        }
+
+        private void DrawTracks()
+        {
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            {
+                EditorGUILayout.LabelField("Audio Tracks", EditorStyles.largeLabel);
+                if (GUILayout.Button("Add", GUILayout.Width(buttonWidth)))
+                {
+                    trackArraySize.intValue++;
+                    tracks.GetArrayElementAtIndex(trackArraySize.intValue - 1).objectReferenceValue = null;
+                }
+                EditorGUI.indentLevel++;
+                EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+                {
+                    for (int i = 0; i < trackArraySize.intValue; i++)
+                    {
+                        SerializedProperty track = tracks.GetArrayElementAtIndex(i);
+                        EditorGUILayout.BeginHorizontal();
+                        {
+                            string label = string.Format("Track {0}", i.ToString());
+                            EditorGUILayout.PropertyField(track, new GUIContent(label));
+                            if (GUILayout.Button("Remove", GUILayout.Width(buttonWidth)))
+                            {
+                                track.objectReferenceValue = null;
+                                tracks.DeleteArrayElementAtIndex(i);
+                            }
+                        }
+                        EditorGUILayout.EndHorizontal();
+                    }
+                }
+                EditorGUILayout.EndVertical();
+                EditorGUI.indentLevel--;
+            }
+            EditorGUILayout.EndVertical();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(loop);
+            EditorGUILayout.PropertyField(playOnAwake);
+            EditorGUILayout.PropertyField(output);
+            DrawTracks();
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
+
+#endif
