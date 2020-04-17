4f4d10574e9c77bfe007053996a3319216edaca0
Famous <clark.famous@gmail.com>
2020-04-17 11:50:25 -0400

    Basic AR foundaton scripts for IOS

---
+++ Assets/Scripts/ARCameraPosTracking.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARCameraPosTracking : MonoBehaviour
+{
+
+    private Vector3 ARCameraPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ARCameraPos = transform.position;
+        Debug.Log("X coordinate: [" + ARCameraPos.x + "] " + "Y coordinate: [" + ARCameraPos.y + "] " + "Z coordinate: [" + ARCameraPos.z + "]" );
+    }
+}
---
+++ Assets/Scripts/ARImageRecgonition/ImageRecognition.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageRecognition : MonoBehaviour
+{
+
+    //To implement in Unity Scene:
+
+    //(1) For this script to work, you must add the component: `AR Tracked Image Manger` to the `AR Session Origin` Compoent 
+    //(2) Public variable `Serilaized Library` needs an XR `Reference Image Library`
+    //(3) Public variable `Tracked Image Prefab` needs any prefab that you wwant to appear when an image is detected
+
+    private ARTrackedImageManager _arTrackedImageManager;
+
+    private void Awake()
+    {
+        _arTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    public void OnEnable()
+    {
+        _arTrackedImageManager.trackedImagesChanged += OnImageChanged;    
+    }
+
+    public void OnDisable()
+    {
+        _arTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var trackedImage in args.added)
+        {
+            Debug.Log(trackedImage.name);
+        }
+    }
+}
